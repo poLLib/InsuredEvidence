f869fc4de98c16266ab97eb1958c3344854f018a
plb-ubuntu <pollib161@gmail.com>
2023-12-03 17:36:59 +0100

    correction of UI and Main class difference

--- src/main/java/bb/example/Main.java
+++ src/main/java/bb/example/Main.java
@@ -10,44 +10,6 @@ public class Main {
         /* Start of the user session loop
            The user selects a command by entering a number
          */
-        boolean end = false;
-        while (!end) {
-            System.out.printf(userInterface.displayMenu()); // Displays the main menu
-            try {
-                int input = userInterface.menuSelection();
-                if (input >= 1 && input <= 7) {
-                    switch (input) {
-
-                        /* Command to register a new insured person --> @name @surname @tel @age */
-                        case 1 -> userInterface.addInsured();
-
-                        /* Command to list all insured individuals */
-                        case 2 -> userInterface.displayAllInsured();
-
-                        /* Command to search for a specific insured person by name or surname content --> @name @surname */
-                        case 3 -> userInterface.displayInsured();
-
-                        /* Command to modify insured person's data / search by name and surname */
-                        case 4 -> userInterface.modifyInsured();
-
-                        /* Command to delete a specific insured person */
-                        case 5 -> userInterface.deleteInsured();
-                        /*
-                         *//* Command to save the list of insured persons to the file.txt *//*
-                        case 6 -> userInterface.createFile();*/
-
-                        /* Command to end the loop */
-                        case 7 -> {
-                            System.out.printf(userInterface.endProgram());
-                            end = true;
-                        }
-                    }
-                } else {
-                    System.out.println("Enter a number from 1 to 7"); // Output for incorrect command, out of range of 1 - 7
-                }
-            } catch (Exception e) {
-                System.out.println("Enter a number from 1 to 7"); // Output for incorrect command, null or symbols
-            }
-        }
+        userInterface.menuSelection();
     }
 }
--- src/main/java/bb/example/UserInterface.java
+++ src/main/java/bb/example/UserInterface.java
@@ -61,10 +61,47 @@ public class UserInterface {
      *
      * @return - returns the numeric input
      */
-    public int menuSelection() {
-        return Integer.parseInt(sc.nextLine());
-    }
+    public void menuSelection() {
+        boolean end = false;
+        while (!end) {
+            System.out.printf(displayMenu()); // Displays the main menu
+            try {
+                int input = Integer.parseInt(sc.nextLine().trim());
+                if (input >= 1 && input <= 7) {
+                    switch (input) {
 
+                        /* Command to register a new insured person --> @name @surname @tel @age */
+                        case 1 -> addInsured();
+
+                        /* Command to list all insured individuals */
+                        case 2 -> displayAllInsured();
+
+                        /* Command to search for a specific insured person by name or surname content --> @name @surname */
+                        case 3 -> displayInsured();
+
+                        /* Command to modify insured person's data / search by name and surname */
+                        case 4 -> modifyInsured();
+
+                        /* Command to delete a specific insured person */
+                        case 5 -> deleteInsured();
+                        /*
+                         *//* Command to save the list of insured persons to the file.txt *//*
+                        case 6 -> createFile();*/
+
+                        /* Command to end the loop */
+                        case 7 -> {
+                            System.out.printf(endProgram());
+                            end = true;
+                        }
+                    }
+                } else {
+                    System.out.println("Enter a number from 1 to 7"); // Output for incorrect command, out of range of 1 - 7
+                }
+            } catch (Exception e) {
+                System.out.println("Enter a number from 1 to 7"); // Output for incorrect command, null or symbols
+            }
+        }
+    }
     /**
      * User input to add a new insured person to the database
      */
