b2a1ac36ffff3212e11cd572f2a603fc01af20d8
plb-ubuntu <pollib161@gmail.com>
2024-02-20 14:02:48 +0100

    update PersonI, static inner class PersonImlp

---
+++ src/main/java/bb/example/PersonI.java
@@ -0,0 +1,17 @@
+package bb.example;
+
+public interface PersonI {
+    String getName();
+
+    void setName(String newName);
+
+    String getSurname();
+
+    void setSurname(String surname);
+
+    String getPhone();
+
+    void setPhone(String phone);
+
+    int getAge();
+}
