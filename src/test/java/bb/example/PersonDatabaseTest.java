9f0ef0bbb497a5bf4fe00ef1760aeefe75de6ca5
plb-ubuntu <pollib161@gmail.com>
2024-02-28 16:41:05 +0100

    assertation PersonDatabaseTest

--- src/test/java/bb/example/PersonDatabaseTest.java
+++ src/test/java/bb/example/PersonDatabaseTest.java
@@ -1,36 +1,18 @@
 package bb.example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 class PersonDatabaseTest {
-    @Mock
-    PersonI mockedPerson;
-    @InjectMocks
+
     PersonDatabase database;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
-    void testAddPerson() {
-        when(mockedPerson.getId()).thenReturn(1);
+    void addingNewPersonIntoDatabaseTest() {
         database.addPerson("John", "Doe", "123456789", 25);
-
-        verify(mockedPerson).setName("John");
-        verify(mockedPerson).setSurname("Doe");
-        verify(mockedPerson).setPhone("123456789");
-        verify(mockedPerson).setAge(25);
-
         PersonI addedPerson = database.findById(1);
 
         assertThat(addedPerson).isNotNull();
@@ -38,5 +20,59 @@ class PersonDatabaseTest {
         assertThat(addedPerson.getSurname()).isEqualTo("Doe");
         assertThat(addedPerson.getPhone()).isEqualTo("123456789");
         assertThat(addedPerson.getAge()).isEqualTo(25);
+    }
+
+    @Test
+    void sizeOfDatabaseTest() {
+        database.addPerson("John", "Doe", "123456789", 25);
+        database.addPerson("Jim", "Jarmush", "753159654", 49);
+        database.addPerson("Stanley", "Kubrick", "357159456", 60);
+        database.addPerson("Tim", "Burton", "789654123", 33);
+
+        Collection<PersonI> listPersons = database.listOfAllPersons();
+
+        assertThat(listPersons.size()).isEqualTo(4);
+    }
+
+    @Test
+    void searchingForPersonInDatabaseByPartOfNameOrSurnameStringTest() {
+        database.addPerson("John", "Doe", "123456789", 25);
+        database.addPerson("Jim", "Jarmush", "753159654", 49);
+        database.addPerson("Stanley", "Kubrick", "357159456", 60);
+        database.addPerson("Tim", "Burton", "789654123", 33);
+        database.addPerson("Luke", "Bimbo", "789654123", 33);
+
+        Collection<PersonDatabase.PersonImpl> foundPersons = database.findSpecificPerson("im");
+
+        assertThat(foundPersons.size()).isEqualTo(3);
+    }
+
+    @Test
+    void editingNameSurnameAndPhoneNumberString() {
+        database.addPerson("John", "Doe", "123456789", 25);
+
+        database.editPerson(1, "Honza", "Řezník", "735845657");
+        PersonI editedPerson = database.findById(1);
+
+        assertThat(editedPerson.getId()).isEqualTo(1);
+        assertThat(editedPerson.getName()).isEqualTo("Honza");
+        assertThat(editedPerson.getSurname()).isEqualTo("Řezník");
+        assertThat(editedPerson.getPhone()).isEqualTo("735845657");
+    }
+
+    @Test
+    void deletingPersonTest() {
+        database.addPerson("John", "Doe", "123456789", 25);
+        database.addPerson("Jim", "Jarmush", "753159654", 49);
+        database.addPerson("Stanley", "Kubrick", "357159456", 60);
+
+        database.deletePerson(1);
+        Collection<PersonI> databaseAfterDelete = database.listOfAllPersons();
+        PersonI personIdTwo = database.findById(2);
+
+        assertThat(databaseAfterDelete.size()).isEqualTo(2);
+        assertThat(personIdTwo.getId()).isEqualTo(2);
+        assertThat(personIdTwo.getName()).isEqualTo("Jim");
+
     }
 }
