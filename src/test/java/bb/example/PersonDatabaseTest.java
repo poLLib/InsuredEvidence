dff65fff052ef11bb55cf1dd5073c106cf0f8a08
Octavio Calleya <ottocg@protonmail.com>
2024-02-28 12:53:09 +0100

    Fix test

--- src/test/java/bb/example/PersonDatabaseTest.java
+++ src/test/java/bb/example/PersonDatabaseTest.java
@@ -1,42 +1,44 @@
 package bb.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class PersonDatabaseTest {
+
     @Mock
-    PersonI mockedPerson;
-    @InjectMocks
+    PersonI person;
+
     PersonDatabase database;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    void beforeEach() {
+        database = new PersonDatabase();
     }
 
     @Test
-    void testAddPerson() {
-        when(mockedPerson.getId()).thenReturn(1);
+    @DisplayName("Should add person to database")
+    void shouldAddPersonTest() {
+        PersonI addedPerson = database.findById(1);
+        assertThat(addedPerson).isNull();
+
         database.addPerson("John", "Doe", "123456789", 25);
 
-        verify(mockedPerson).setName("John");
-        verify(mockedPerson).setSurname("Doe");
-        verify(mockedPerson).setPhone("123456789");
-        verify(mockedPerson).setAge(25);
-
-        PersonI addedPerson = database.findById(1);
+        addedPerson = database.findById(1);
 
         assertThat(addedPerson).isNotNull();
         assertThat(addedPerson.getName()).isEqualTo("John");
         assertThat(addedPerson.getSurname()).isEqualTo("Doe");
         assertThat(addedPerson.getPhone()).isEqualTo("123456789");
         assertThat(addedPerson.getAge()).isEqualTo(25);
+
+        verifyNoMoreInteractions(person);
     }
 }
